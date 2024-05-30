c2fc9606b0e236d9f84d5487af2dbc8c08266904
Doha Kaddaf <doha.kaddaf@gmail.com>
2024-05-30 21:26:20 +0200

    complete lab

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(`${__dirname}/views/about.html`);
+    });
+
+app.get("/works", (req, res) => {
+    res.sendFile(`${__dirname}/views/works.html`);
+    });
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
